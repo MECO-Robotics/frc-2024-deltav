29ca0a08819c389542455d34a836e5b164388c98
restofnate <restofnate@gmail.com>
2024-02-09 21:03:54 -0500

    Added arm constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,7 +4,9 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.util.Units;
 import swervelib.math.Matter;
 import swervelib.parser.PIDFConfig;
@@ -51,4 +53,70 @@ public final class Constants
     public static final double RIGHT_X_DEADBAND = 0.01;
     public static final double TURN_CONSTANT = 0.75;
   }
+
+  //Arm does not include shooter it only the beam and the gearboxes attached to super structure
+  public static final class Arm {
+    //Creates the CAN id's for the 2 motor arm gearboxes on the super structure
+    public static final int rightMotorOneCANID = 0;
+    public static final int leftMotorOneCANID = 0;
+    public static final int rightMotorTwoCANDID = 0;
+    public static final int leftMotorTwoCANDID = 0;
+
+    public static final boolean rightMotorOneCANIDInverted = true; //IDK if it needs to be inverted
+    public static final boolean leftMotorOneCANIDInverted = true;
+    public static final boolean rightMotorTwoCANDIDInverted = true;
+    public static final boolean leftMotorTwoCANDIDInverted = true;
+    //only need 1 current limiting since all the motors should have the same current limit
+    public static final int kCurrentLimit = 0; //Sets current limiting
+
+    public static final double kSoftLimitReverse = 0;
+    public static final double kSoftLimitForward = 0.0;
+
+    //IDK the gear ratios so these are place holders
+    public static final double kArmGearRatio = (1.0 / 25.0) * (28.0 / 50.0) * (16.0 / 64.0); 
+    public static final double kPositionFactor =
+        kArmGearRatio
+            * 2.0
+            * Math.PI; // multiply SM value by this number and get arm position in radians
+    public static final double kVelocityFactor = kArmGearRatio * 2.0 * Math.PI / 60.0;
+    public static final double kArmFreeSpeed = 5676.0 * kVelocityFactor;
+    public static final double kArmZeroCosineOffset =
+        1.342; // radians to add to converted arm position to get real-world arm position (starts at
+    // ~76.9deg angle)
+    public static final ArmFeedforward kArmFeedforward =
+        new ArmFeedforward(0.0, 3.0, 12.0 / kArmFreeSpeed, 0.0);
+//    public static final PIDGains kArmPositionGains = new PIDGains();
+    public static final TrapezoidProfile.Constraints kArmMotionConstraint =
+        new TrapezoidProfile.Constraints(1.0, 2.0);
+
+    public static final double kHomePosition = 0.0;
+    public static final double kScoringPosition = 0.0;
+    public static final double kIntakePosition = 0;
+
+    //
+    
+  
+  }
+
+  public static final class Intake {
+    //public static final int kCanId = 0;
+    public static final int intakeMotorCANID = 0;
+    public static final boolean intakeMotorCANIDInverted = false;
+    public static final int kCurrentLimit = 0;
+
+//    public static final PIDGains kPositionGains = new PIDGains();
+    public static final double kPositionTolerance = 0;
+
+    public static final double kIntakePower = 0;
+
+    public static final double kShotFeedTime = 0;
+  }
+
+
+
+  public static final class Shooter{
+    
+  }
+
+
 }
