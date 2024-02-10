fb53050f0d6ec834b48bd839b10e4327d507b9f4
Nathaniel <restofnate@gmail.com>
2024-02-10 14:06:21 -0800

    Arm changes

---
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -0,0 +1,60 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ArmSubsystem extends SubsystemBase {
+    
+    //-----------------AIM/SHOOT-----------------
+
+    //stow
+    public void stowArm() {
+
+    }
+
+    //recieve
+    public void recieveNote() {
+
+    }
+
+    //idle flywheels
+    public void idleFlywheels() {
+
+    }
+
+    //aim speaker/rev flywheels
+    //angle starts at zero, can only increase
+    public boolean aimSpeaker(float angle) {
+
+        return true;
+    }
+
+    //shoot speaker
+    public void shootSpeaker() {
+
+    }
+
+    //aim amp
+    public boolean aimAmp() {
+
+        return true;
+    }
+
+    //shoot amp
+    public void shootAmp() {
+
+    }
+
+    //--------------CLIMB--------------
+    
+    //prepare for climb
+    public void armUp() {
+
+    }
+
+    //switch to manual climb
+    public void manualClimb(float climbRate) {
+
+    }
+
+
+}
---
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -0,0 +1,22 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class IntakeSubsystem extends SubsystemBase {
+
+    // intake initial command
+    public void startIntaking(boolean shooterEmpty, boolean armDown) {
+    
+    }
+
+    // handoff to shooter command
+    public void handoffNote() {
+
+    }
+
+    // emergency outtake command
+    public void ejectIntake(boolean ejectOn) {
+
+    }
+
+}
