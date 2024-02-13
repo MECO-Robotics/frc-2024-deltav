38d7a2e0aa4b70df04a02bba7cae88a0951b4f9e
Paul Foster <79760895+paul-foster@users.noreply.github.com>
2024-02-13 07:55:27 -0500

    Added TODOs for intake subsystem

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -2,8 +2,13 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+// TODO: Add a class comment. What does this class do?
 public class IntakeSubsystem extends SubsystemBase {
 
+    // TODO: Declare the motors used on the intake. Something like:
+    //       private CANSparkMax leftBottomAwesomeMotor = new CANSparkMax(Constants.kSomeCanIDConstant);
+
+    
     // intake initial command
     public void startIntaking(boolean shooterEmpty, boolean armDown) {
     
