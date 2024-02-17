df3d869783f495639ba00e87c49c689c70159a89
Paul Foster <restofpaul@gmail.com>
2024-02-16 19:49:56 -0500

    Added TODO instructions for adding a PID Controller

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -1,8 +1,10 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import frc.robot.Constants;
 
@@ -17,7 +19,41 @@ public class ArmSubsystem extends SubsystemBase {
     private CANSparkMax rightFollowerFlywheelMotor = new CANSparkMax(Constants.Shooter.rightFollowerFlywheelMotor, MotorType.kBrushless);
     private CANSparkMax indexingMotor = new CANSparkMax(Constants.Shooter.indexingMotor, MotorType.kBrushless);
 
-
+    // ------------------------------------------------------------------------------------
+    // TODO: Setup PID Controllers and encoders for (leading) motors. For example:
+    //
+    // 
+    //  GIVEN:  motor is declared as:
+    //
+    //       CANSparkMax motor = new CANSparkMax(...);
+    //
+    //  1. Declare class variables for each
+    //
+    //       PIDController pidController = null;
+    //       RelativeEncoder encoder = null;
+    //
+    //  2. Initialize the variables in the constructor
+    //
+    //       pidController = motor.getPIDController();
+    //       pidController.setP( <some-constant> );
+    //       pidController.setI( <some-constant> );
+    //       pidController.setD( <some-constant> );
+    //       pidController.setI( <some-constant> );
+    //       pidController.setFF( <some-constant> );
+    //       pidController.setOutputRange( <min>, <max> );
+    //
+    //       encoder = motor.getEncoder();
+    //  
+    //  3. Update methods to use the PID Controller by calling the setReference() function:
+    //
+    //       pidController.setReference( <the-desired-position>, ControlType.kPosition);
+    //
+    //  NOTE: The code for velocity PID Control is very similar, just using ControlType.kVelocity instead
+    //        The encoder variable is not actually needed for pid control. The PID Controller on the SparkMAX will get the encoder value directly.
+    //        A more detailed example can be found at:
+    //        https://github.com/REVrobotics/SPARK-MAX-Examples/blob/master/Java/Smart%20Motion%20Example/src/main/java/frc/robot/Robot.java
+    //
+    // ------------------------------------------------------------------------------------
 
     public ArmSubsystem() {
 
@@ -25,7 +61,6 @@ public class ArmSubsystem extends SubsystemBase {
         indexingMotor.setInverted(Constants.Shooter.indexingMotorInverted);
 
         rightFollowerFlywheelMotor.follow(leftLeaderFlywheelMotor, true); 
-        
     }
 
 
@@ -55,6 +90,10 @@ public class ArmSubsystem extends SubsystemBase {
     //aim speaker/rev flywheels
     //angle starts at zero, can only increase
     public boolean aimSpeaker(float angle) {
+
+
+        // TODO: Replace with PID Control following example code from: 
+        //       https://github.com/REVrobotics/SPARK-MAX-Examples/blob/master/Java/Smart%20Motion%20Example/src/main/java/frc/robot/Robot.java
 
         boolean radarLock = setArmPosition(angle);
 
