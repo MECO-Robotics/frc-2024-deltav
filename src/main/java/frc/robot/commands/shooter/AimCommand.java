2c1f31f4e2a2ed0cbce91e7839421810f3012f08
Paul Foster <79760895+paul-foster@users.noreply.github.com>
2024-02-26 15:01:32 +0000

    Added AimCommand

---
+++ src/main/java/frc/robot/commands/shooter/AimCommand.java
@@ -0,0 +1,16 @@
+package frc.robot.commands.shooter;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+/** 
+ * Turn the robot to point to the Speaker, and elevate the arm based on the distance to the speaker.
+ */
+public class AimCommand extends Command {
+    
+
+    // TODO Define class variables for the required subsystems: vision (to receive the aiming direction), swerve (to turn the robot), arm (to raise/lower the arm)
+
+    // TODO Define a constructor that takes in the 3 required subsystems
+
+    // TODO write the execute, end, and isFinished methods
+}
