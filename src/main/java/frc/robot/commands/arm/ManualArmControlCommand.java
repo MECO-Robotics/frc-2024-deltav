dd70f599094560bdb411c2d2121040fb6ad294d0
restofnate <restofnate@gmail.com>
2024-02-29 16:01:57 -0500

    manual control of arm command created

---
+++ src/main/java/frc/robot/commands/arm/ManualArmControlCommand.java
@@ -0,0 +1,25 @@
+package frc.robot.commands.arm;
+
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.ArmSubsystem;
+
+public class ManualArmControlCommand extends Command{
+    
+    private final ArmSubsystem arm;
+    private final DoubleSupplier input;
+
+    public ManualArmControlCommand(ArmSubsystem armSubsystem, DoubleSupplier controllerInput){
+        arm = armSubsystem;
+        input = controllerInput;
+
+    }
+    public void execute(){
+        double returnedValue = input.getAsDouble();
+        arm.manualArmControl(returnedValue);
+    }
+    public boolean isFinished(){
+        return false;
+    }
+}
