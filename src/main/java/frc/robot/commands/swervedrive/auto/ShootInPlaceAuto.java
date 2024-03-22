6ccf7995d6bc9cc4d733657dafb948b4b596955e
restofnate <restofnate@gmail.com>
2024-03-22 12:59:54 -0400

    shoot in place - no more full stop on flywheels

--- src/main/java/frc/robot/commands/swervedrive/auto/ShootInPlaceAuto.java
+++ src/main/java/frc/robot/commands/swervedrive/auto/ShootInPlaceAuto.java
@@ -5,6 +5,7 @@ import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants;
@@ -27,7 +28,7 @@ public class ShootInPlaceAuto extends SequentialCommandGroup {
     new IndexingCommand(indexer, Constants.Indexing.indexingSpeed),
     new WaitCommand(.5),
     new IndexingCommand(indexer, 0),
-    new ShooterCommand(shooter, 0, 0)
+    new InstantCommand(shooter::disable)
     );
     
   }                       
